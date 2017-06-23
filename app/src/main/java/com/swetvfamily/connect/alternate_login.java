216a14d457366977ad0eca911b06a0ee6b0c6a83
Dawminic <dominic.aidoo@gmail.com>
2017-06-23 17:59:41 -0400

    Alt Login Activity - Starting this activity from scratch, simple login layout, no functionality

--- app/src/main/java/com/swetvfamily/connect/alternate_login.java
+++ app/src/main/java/com/swetvfamily/connect/alternate_login.java
@@ -3,11 +3,11 @@ package com.swetvfamily.connect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AltLoginActivity extends AppCompatActivity {
+public class alternate_login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alt_login);
+        setContentView(R.layout.activity_alternate_login);
     }
 }
