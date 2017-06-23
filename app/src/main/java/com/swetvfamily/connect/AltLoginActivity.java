d56dd7ff152c04522859aa6cabd550473f72d3d0
Dawminic <dominic.aidoo@gmail.com>
2017-06-22 22:29:27 -0400

    Alt Login Activity - Starting this activity from scratch

---
+++ app/src/main/java/com/swetvfamily/connect/AltLoginActivity.java
@@ -0,0 +1,13 @@
+package com.swetvfamily.connect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AltLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alt_login);
+    }
+}
